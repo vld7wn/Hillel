a378326f14021c8a5d43e9ae8a3aaa4fe40143ff
vld7wn <52673140+vld7wn@users.noreply.github.com>
2021-05-20 21:32:32 +0300

    Fix

--- FirstDay/FirstHomeWork.java
+++ FirstDay/FirstHomeWork.java
@@ -1,12 +1,19 @@
 package FirstDay;
 
 public class FirstHomeWork {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0) System.out.println("Hello");
-            else if (i % 5 == 0) System.out.println("World");
-            else if (i % 3 == 0 && i % 5 == 0) System.out.println("HelloWorld");
-            else System.out.println(i);
+            if (i % 3 == 0) {
+                System.out.print("Hello ");
+                if (i % 5 == 0) {
+                    System.out.print("World ");
+                    if (i % 3 == 0 && i % 5 == 0) {
+                        System.out.print("HelloWorld");
+                    }
+                }
+            } else {
+                System.out.print(" " + i + " ");
+            }
         }
     }
 }
