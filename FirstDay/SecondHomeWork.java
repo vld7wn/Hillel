c90aa19418e61420d1238fb645fd60db2c300e58
vld7wn <52673140+vld7wn@users.noreply.github.com>
2021-05-20 21:31:58 +0300

    Little Fix

--- FirstDay/SecondHomeWork.java
+++ FirstDay/SecondHomeWork.java
@@ -1,11 +1,23 @@
 package FirstDay;
 
+import java.util.Random;
+
 public class SecondHomeWork {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Random r = new Random();
         int[] number = new int[10];
-        for(int i = 0; i < number.length;i++)
-            number[i] = (int) (Math.random() * 10 + 10);
-       float average = (float)Math.sqrt(number.length);
+        int sum = 0;
+
+        for (int i = 0; i < number.length; i++) {
+//            number[i] = (int) (Math.random() * 11 + 10);
+            number[i] = r.nextInt(11) + 10;
+            sum += number[i];
+//            System.out.println(number[i]);
+        }
+
+        int average = sum / number.length;
+
+//        System.out.println(sum + " Не в цикле");
         System.out.println(average);
     }
 }
